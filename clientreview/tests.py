cb7d57e4808fbaf918833b7052094004f160b4fb
覃家援 <wxqinjiayuan@gf.com.cn>
2023-11-16 15:36:42 +0800

    已完成tst环境脚本

--- clientreview/tests.py
+++ clientreview/tests.py
@@ -1,11 +1,11 @@
 import logging
-from djangoProject.logger import logger
+# from djangoProject.logger import logger
 from django.test import TestCase
 
 # Create your tests here.
-# from django.utils import log
-
-log = logger()
+from django.utils import log
+import logging
+log = logging.getLogger('django.request')
 def some_view():
     log.info('123123')
 some_view()
